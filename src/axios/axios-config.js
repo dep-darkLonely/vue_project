e83eeaca46f87ba3a88843f4927b3be180f82608
hanbin <978637659@qq.com>
2018-09-20 00:17:12 +0800

    refs #1 add axios configure parameter

--- src/axios/axios-config.js
+++ src/axios/axios-config.js
@@ -1,5 +1,15 @@
 /**
- * Description: axios config file.
+ * Description: axios global config file.
  * Date: 2018-9-18
  * Author: huan7
  */
+
+// baseurl
+axios.defaults.baseURL = "https://localhost:8181/"
+
+// connection timeout default 2.5s
+axios.defaults.timeout = 2500
+
+// request interceptor
+
+// response interceptor
